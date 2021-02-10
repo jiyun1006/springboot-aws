0b3a502a0f80c30ee2c1a37cfe1e9e7a3a731970
jiyun1006 <kimjiyoon1006@hanmail.net>
2021-02-11 03:36:48 +0900

    <docs> 파일 추가
    lombok을 이용해서 dto 클래스 파일 생성 및 기존 파일 리팩토링한 파일들 추가.

---
+++ src/test/java/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,19 @@
+package springboot.web.dto;
+
+import com.jiyun.book.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트(){
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
