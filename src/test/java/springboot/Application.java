18454a123f3f0f4f2ea25d68d6f01759e6a666e2
jiyun1006 <kimjiyoon1006@hanmail.net>
2021-02-11 01:21:59 +0900

    <docs> 단위테스트
    단위테스트를 이용해서 기본적인 controller 테스트 코드 작성

---
+++ src/test/java/springboot/Application.java
@@ -0,0 +1,12 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // 스프링부트의 자동 설정, 스프링Bean 읽기와 생성 모두 자동으로 설정.
+public class Application {
+    public static void main(String[] args) {
+        // 내장 was로 별도의 외부 was를 두지않고, 애플리케이션을 실행할 때 내부에서 was를 실
+        SpringApplication.run(Application.class, args);
+    }
+}
